e590530331b0ad725486897f0f3a19f79bb006f0
koksoya <b.koksoya@gmail.com>
2019-07-29 08:29:34 +0200

    Vets and Created and convert into jpa entities. closes #37

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -0,0 +1,45 @@
+package guru.springframework.sfgpetclinic.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Table("visits")
+public class Visit extends BaseEntity{
+
+    @Column(name = "date")
+    private LocalDate date;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToOne
+    private Pet pet;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
